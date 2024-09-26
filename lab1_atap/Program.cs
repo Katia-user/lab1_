d6946d4461214c69a5ce80b78de02d4bfe819d84
Катя <@DESKTOP-8CVJE59>
2024-09-26 20:51:07 +0300

    Add project files.

---
+++ lab1_atap/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_atap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть Х = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y, a, b;
+            a = 2.8;
+            b = -0.3;
+            if (x < 1.2)
+                y = a * Math.Pow(x,2) + b * x;
+            else if (x == 1.2)
+            {
+                y = a/x + Math.Sqrt(Math.Pow(x, 2) + 1);
+            }
+            else
+            {
+                y = (a+b*x)/Math.Sqrt(Math.Pow(x, 2) + 1);
+            }
+            Console.Write(y);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab1_atap222/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_atap222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int k = 1; k <= 99; k++)
+            {
+                if (k % 100 >= 11 && k % 100 <= 19)
+                {
+                    Console.WriteLine($"Менi {k} рокiв");
+                }
+                else
+                {
+                    int lastNumber = k % 10;
+
+                    if (lastNumber == 1)
+                    {
+                        Console.WriteLine($"Менi {k} рiк");
+                    }
+                    else if (lastNumber >= 2 && lastNumber <= 4)
+                    {
+                        Console.WriteLine($"Менi {k} роки");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Менi {k} рокiв");
+                    }
+                }
+            }
+
+        }
+    }
+}
